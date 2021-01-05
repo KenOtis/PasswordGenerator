11562fa8a2c2bf5113f688dab05624082722a329
KenOtis <66284239+KenOtis@users.noreply.github.com>
2021-01-05 14:15:19 -0500

    Add files via upload

---
+++ PasswordGen.py
@@ -0,0 +1,19 @@
+#Kenneth Otis
+#Python Passowrd Generator
+
+
+def PasswordGenerator():
+    reason=str(input("What would you like the password for? "))
+    length=int(input("How long would you like your password to be? "))
+    low=['a',"b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    upp=['A',"B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    numbers=[0,1,2,3,4,5,6,7,8,9,0]
+    Special=["$","%","&"]
+    alls=low+upp+numbers+Special
+    password=random.sample(alls,length)
+    print ("Your new ",reason, " password: ",*password,sep="")
+PasswordGenerator()
+
+    
+
+
